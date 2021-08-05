8eb945e1f095a7dccf41daedd78dfdb2db829c40
8hjgh <88498642+8hjgh@users.noreply.github.com>
2021-08-06 00:43:44 +1000

    Update budugssh.sh

--- budugssh.sh
+++ budugssh.sh
@@ -1,6 +1,6 @@
 !pip install colab_ssh --upgrade
 
 from colab_ssh import launch_ssh
-launch_ssh('1wDFGvZO9fWYLoOWQSut7BojnW1_8Qv2chYwrj9Ccu4rwXCJ', '1')
+launch_ssh('1wDfldS3Z7uzgTpSzab9GN7PkwD_6sTa4U3K2qxsstT3S8Tzd', '1')
 
 !sleep 99999
