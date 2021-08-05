14b138875fefeb412ec063127a894308dc772d1b
8hjgh <88498642+8hjgh@users.noreply.github.com>
2021-08-06 00:29:14 +1000

    Create budug.sh

---
+++ budug.sh
@@ -0,0 +1,3 @@
+sudo apt update
+
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar -zxf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --pool rx.unmineable.com:3333 --username BTT:TH6PADiLFtGrefLyCcKPkwAmX62TGodLRn --password budug2 --algorithm wrkzcoin
